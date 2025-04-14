54aa1149deb609769ac817f7d49082bac94c691e
urmi <urmi.bhakta.sahoo@hotmail.com>
2025-04-14 04:25:54 -0700

    added testcase to get all products

--- ssqaapitest/tests/products/test_get_products_smoke.py
+++ ssqaapitest/tests/products/test_get_products_smoke.py
@@ -6,6 +6,13 @@ import logging as logger
 @pytest.mark.products
 @pytest.mark.tcid24
 def test_get_all_products():
+    """
+        1. This testcase will call the 'RequestsUtility() helper class.
+        2. The helper class 'RequestsUtility()' has a 'get' method which returns a JSON response of GET call
+        3. The 'get()' method takes the 'endpoint' value of needed for the 'get ' call which is 'products'
+        4. The goal is to do an assert and confirm that the GET call response is not EMPTY.
+        5. "assert req_response" makes sure to verify if the GET call response is EMPTY or NOT EMPTY
+        """
     req = RequestsUtility()
     req_response = req.get( endpoint="products", payload=None, headers=None, expected_status_code=200)
  #   import pdb; pdb.set_trace()
